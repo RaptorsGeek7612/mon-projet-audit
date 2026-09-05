5a9963f62f4f3d50acbdb648e0fee990bae7d56b
RaptorsGeek7612 <sherp91@gmail.com>
2026-09-05 21:08:10 +0200

    debug: test passing a concrete-typed instance directly (Solidity implicit interface conversion)

--- test/halmos/Isolation.halmos.sol
+++ test/halmos/Isolation.halmos.sol
@@ -3,8 +3,20 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 
-contract Helper {
+interface IHelperIface {
+    function x() external view returns (uint256);
+}
+
+contract Helper is IHelperIface {
     uint256 public x;
+}
+
+contract ConsumesInterfaceDirectly {
+    IHelperIface public immutable target;
+
+    constructor(IHelperIface _target) {
+        target = _target;
+    }
 }
 
 contract NoImmutable {
@@ -40,17 +52,20 @@ contract IsolationHalmosTest is Test {
     NoImmutable a;
     WithImmutableAddress b;
     WithImmutableInterface c;
+    ConsumesInterfaceDirectly d;
 
     function setUp() public {
         helper = new Helper();
         a = new NoImmutable(address(helper));
         b = new WithImmutableAddress(address(helper));
         c = new WithImmutableInterface(address(helper));
+        d = new ConsumesInterfaceDirectly(helper);
     }
 
     function check_dummy() public view {
         assert(a.target() == address(helper));
         assert(b.target() == address(helper));
         assert(address(c.target()) == address(helper));
+        assert(address(d.target()) == address(helper));
     }
 }
